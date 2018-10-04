9c445c2eb2c93e7bcda56ff3087085e9e07dda8e
Iord Neto <iordneto95@gmail.com>
2018-10-04 19:15:57 -0300

    Fix the operators

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const FizzBuzz = number => {
-  if (number == 0) return number;
-  if (number % 3 == 0 && number % 5 == 0) return 'FizzBuzz';
-  if (number % 3 == 0) return 'Fizz';
-  if (number % 5 == 0) return 'Buzz';
+  if (number === 0) return number;
+  if (number % 3 === 0 && number % 5 == 0) return 'FizzBuzz';
+  if (number % 3 === 0) return 'Fizz';
+  if (number % 5 === 0) return 'Buzz';
 
   return number;
 };
